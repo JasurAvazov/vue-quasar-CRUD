ce940935ef598bff413ff333ee47c3208d3556c2
JasurAvazov <jasuravazov4@gmail.com>
2023-05-17 15:28:18 +0500

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p>{{ $store.state.count }}</p>
+    <button @click="increase">dsad</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    increase() {
+      this.$store.state.count++
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
